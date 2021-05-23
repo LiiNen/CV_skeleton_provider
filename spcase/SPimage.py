710c2329f384df3e16411e3d1b9d394bd2a20f64
Seunggyun98 <dmstmdrbs@ajou.ac.kr>
2021-05-23 14:04:35 +0900

    Add changing gamma func

--- spcase/SPimage.py
+++ spcase/SPimage.py
@@ -3,6 +3,16 @@ import cv2
 import time
 import numpy as np
 
+
+def gamma(frame):
+    g = float(input("감마 값 : "))
+    out = frame.copy()
+    out = frame.astype(np.float)
+    out = ((out / 255) ** (1 / g)) * 255
+    out = out.astype(np.uint8)
+    return out
+
+
 def forImage(opt):
     print('img')
     source, skeleton_bool, keypoint_bool, exclude, weightsFile, protoFile, threshold = opt.source, opt.skel, opt.keyp, opt.exclude, opt.weight, opt.proto, opt.thres
@@ -14,9 +24,11 @@ def forImage(opt):
                 return
 
     nPoints = 18
-    POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[1,8],[8,9],[9,10],[1,11],[11,12],[12,13],[0,14],[0,15],[14,16],[15,17]]
+    POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [
+        8, 9], [9, 10], [1, 11], [11, 12], [12, 13], [0, 14], [0, 15], [14, 16], [15, 17]]
 
     frame = cv2.imread(source)
+    frame = gamma(frame)
     frameCopy = np.copy(frame)
     frameWidth = frame.shape[1]
     frameHeight = frame.shape[0]
@@ -28,10 +40,9 @@ def forImage(opt):
     inWidth = 368
     inHeight = 368
     inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
-                            (0, 0, 0), swapRB=False, crop=False)
+                                    (0, 0, 0), swapRB=False, crop=False)
 
     net.setInput(inpBlob)
-
     output = net.forward()
     print("time taken by network : {:.3f}".format(time.time() - t))
 
@@ -43,18 +54,20 @@ def forImage(opt):
     for i in range(nPoints):
         probMap = output[0, i, :, :]
         minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-        
+
         # 원본이미지 좌표에 대입
         x = (frameWidth * point[0]) / W
         y = (frameHeight * point[1]) / H
 
         # threshold 넘는 것만 keypoint 저장
-        if prob > threshold : 
-            cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-            cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+        if prob > threshold:
+            cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255),
+                       thickness=-1, lineType=cv2.FILLED)
+            cv2.putText(frameCopy, "{}".format(i), (int(x), int(
+                y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
 
             points.append((int(x), int(y)))
-        else :
+        else:
             points.append(None)
 
     # 원본 이미지 위에 그리기
@@ -64,8 +77,8 @@ def forImage(opt):
 
         if points[partA] and points[partB]:
             cv2.line(frame, points[partA], points[partB], (0, 255, 255), 2)
-            cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-
+            cv2.circle(frame, points[partA], 8, (0, 0, 255),
+                       thickness=-1, lineType=cv2.FILLED)
 
     cv2.imshow('output_keypoints', frameCopy)
     cv2.imshow('output_skeleton', frame)
