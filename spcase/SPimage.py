e49fbdac3e61684d7eea22cfa87ff90e7449fbd7
Seunggyun98 <dmstmdrbs@ajou.ac.kr>
2021-05-25 15:08:06 +0900

    Delete unused code

--- spcase/SPimage.py
+++ spcase/SPimage.py
@@ -5,49 +5,9 @@ import time
 import numpy as np
 
 
-# def deleteShadow(frame):
-#     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-#     out = cv2.adaptiveThreshold(
-#         frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 5)
-#     out = cv2.cvtColor(out, cv2.COLOR_GRAY2RGB)
-#     return out
-
-
-# def hogDetector(frame):
-#     HOGCV = cv2.HOGDescriptor()
-#     HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-#     bounding_box_cordinates, weights = HOGCV.detectMultiScale(
-#         frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
-
-#     person = 1
-#     for x, y, w, h in bounding_box_cordinates:
-#         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-#         cv2.putText(frame, f'person {person}', (x, y),
-#                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-#         person += 1
-
-#     cv2.putText(frame, 'Status : Detecting ', (40, 40),
-#                 cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
-#     cv2.putText(frame, f'Total Persons : {person-1}',
-#                 (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
-#     cv2.imshow('output', frame)
-
-#     return frame
-
-
-# # 의미 없음
-# def flipColor(frame):
-#     return 255-frame
-
 def findWhite(frame):
-    # ROI세팅 - 사람이 존재하는 영역 찾기
-    # area=width*height
-    # lower_white = np.array([0, 0, 0], dtype=np.uint8)
-    # upper_white = np.array([0, 0, 255], dtype=np.uint8)
     lower_white = np.array([0, 0, 168])
     upper_white = np.array([172, 111, 255])
-    # mask_gray = cv2.inRange(frame, lower_white, upper_white)
-    # img_res = cv2.bitwise_and(frame, frame, mask=mask_gray)
     # Hue is from 0-179 for Opencv
     # Set minimum and max HSV values to display
     # Create HSV Image and threshold into a range.
@@ -60,9 +20,6 @@ def findWhite(frame):
     # Arrange all pixels into a tall column of 3 RGB values and find unique rows (colours)
     colours, counts = np.unique(
         output.reshape(-1, 3), axis=0, return_counts=1)
-    # # print(colours[0])
-    # black_proportion = (100*counts[0])/(h*w)
-    # print(black_proportion)
 
     # # Iterate through unique colours
     for index, colour in enumerate(colours):
@@ -77,7 +34,6 @@ def gamma(frame):
     out = frame.astype(np.float)
     out = ((out / 255) ** (1 / g)) * 255
     out = out.astype(np.uint8)
-    # out = hogDetector(out)
     return out
 
 
@@ -104,13 +60,10 @@ def forImage(opt):
 
     frame = cv2.imread(source)
     frame = gamma(frame)
-    # frame = flipColor(frame)
-    # frame = imutils.resize(frame, width=min(800, frame.shape[1]))
-    # frame = equalize(frame)
-    # frame = deleteShadow(frame)
+
     black_proportion = findWhite(frame)
-    # if(black_proportion < 30):
-    frame = equalize(frame)
+    if(black_proportion < 30):
+        frame = equalize(frame)
     frameCopy = np.copy(frame)
     frameWidth = frame.shape[1]
     frameHeight = frame.shape[0]
@@ -121,8 +74,6 @@ def forImage(opt):
     # 네트워크 인풋 사이즈 설정
     inWidth = 368
     inHeight = 368
-    # inWidth = 720
-    # inHeight = 720
     inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
                                     (0, 0, 0), swapRB=False, crop=False)
 
