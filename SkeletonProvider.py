6ee2c010bb7bd7a1387f78b7eb9e6a34dc3f15a9
Seunggyun98 <dmstmdrbs@ajou.ac.kr>
2021-05-25 15:01:10 +0900

    Add findWhiteProportion function

--- SkeletonProvider.py
+++ SkeletonProvider.py
@@ -9,17 +9,25 @@ from utils.formatter import str2bool
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--source', type=str, default='example.jpeg', help='input source path. default example.jpeg')
-    parser.add_argument('--skel', type=str2bool, default=True, help='if you want not to draw skeleton, set arg to False')
-    parser.add_argument('--keyp', type=str2bool, default=True, help='if you want not to draw ketpoints, set arg to False')
-    parser.add_argument('--exclude', nargs='+', type=int, default=-1, help='points to exclude. args for spacing. -1 for none(default), 0~17 to exclude. else error')
-    parser.add_argument('--proto', type=str, default='pose/coco/pose_deploy_linevec.prototxt', help='for model. default pose/coco/pose_deploy_linevec.prototxt')
-    parser.add_argument('--weight', type=str, default='pose/coco/pose_iter_440000.caffemodel', help='for model. default pose/coco/pose_iter_440000.caffemodel')
-    parser.add_argument('--thres', type=float, default=0.1, help='set threshold for detecting. default 0.1')
-    parser.add_argument('--gray', type=str2bool, default=False, help='preprocessing using gray img, set True')
+    parser.add_argument('--source', type=str, default='example.jpeg',
+                        help='input source path. default example.jpeg')
+    parser.add_argument('--skel', type=str2bool, default=True,
+                        help='if you want not to draw skeleton, set arg to False')
+    parser.add_argument('--keyp', type=str2bool, default=True,
+                        help='if you want not to draw ketpoints, set arg to False')
+    parser.add_argument('--exclude', nargs='+', type=int, default=-1,
+                        help='points to exclude. args for spacing. -1 for none(default), 0~17 to exclude. else error')
+    parser.add_argument('--proto', type=str, default='pose/coco/pose_deploy_linevec.prototxt',
+                        help='for model. default pose/coco/pose_deploy_linevec.prototxt')
+    parser.add_argument('--weight', type=str, default='pose/coco/pose_iter_440000.caffemodel',
+                        help='for model. default pose/coco/pose_iter_440000.caffemodel')
+    parser.add_argument('--thres', type=float, default=0.1,
+                        help='set threshold for detecting. default 0.1')
+    parser.add_argument('--gray', type=str2bool, default=False,
+                        help='preprocessing using gray img, set True')
     opt = parser.parse_args()
     print(opt)
-    
+
     if opt.source.split('.')[1] in ['jpg', 'jpeg', 'png']:
         forImage(opt)
     elif opt.source.split('.')[1] in ['mp4', 'avi', 'mkv']:
